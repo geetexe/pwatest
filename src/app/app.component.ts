d290c0d94ee98f15d6dd244ca6804360e77e9249
Geet <geet.exe@gmail.com>
2021-06-13 02:54:02 +0530

    added sw methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { fader } from './route-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    fader
-  ]
+  animations: [ fader ]
 })
 export class AppComponent {
+
+  constructor(private update: SwUpdate){
+    this.updateClient();
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData;
+  }
+
+  updateClient(){
+    if(!this.update.isEnabled){
+      console.log("update is not enabled!");
+    }
+    this.update.available.subscribe(
+      event => {
+        console.log(`current> ${event?.current}`);
+        console.log(`available> ${event?.available}`);
+      }
+    )
   }
 }
